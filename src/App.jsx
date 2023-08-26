d42025266bea8afa68bcfeaa8e826423d4dd6911
qasim29 <qasimmohammad534@gmail.com>
2023-08-26 22:24:39 +0500

    did auth with class

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ClerkProviderWithRoutes from "./router/ClerkProviderWithRoutes";
-import.meta.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
 
-// if (!REACT_APP_CLERK_PUBLISHABLE_KEY) {
-//   throw new Error("Missing Publishable Key");
-// }
+if (!clerkPubKey) {
+  throw new Error("Missing Publishable Key");
+}
 
-// const clerkPubKey = REACT_APP_CLERK_PUBLISHABLE_KEY;
-// console.log(REACT_APP_CLERK_PUBLISHABLE_KEY);
 function App() {
   return (
     <BrowserRouter>
-      <ClerkProviderWithRoutes clerkPubKey="pk_test_c291bmQtc2hlZXBkb2ctMzQuY2xlcmsuYWNjb3VudHMuZGV2JA" />
+      <ClerkProviderWithRoutes clerkPubKey={clerkPubKey} />
     </BrowserRouter>
   );
 }
